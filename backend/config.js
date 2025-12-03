9da1892ca32cd8c4ce30d895bee7d2a993e4b7e3
Mondal2002 <96198678+Mondal2002@users.noreply.github.com>
2025-12-03 13:00:59 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,1 @@
-export const PORT=5555;
-export const MongoDBURL='mongodb+srv://subhammondal735_db_user:RGUX7TQo7O4m69qw@book-store2.1oxycwm.mongodb.net/?appName=Book-store2'
 
-//password for bookdata RGUX7TQo7O4m69qw
