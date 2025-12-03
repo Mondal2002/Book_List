042dfd7c8115500dec1457270eda62cea4649601
Mondal2002 <subhammondal735@gmail.com.com>
2025-12-03 18:34:06 +0530

    Cors Issue fix

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-<<<<<<< HEAD
       "https://book-list-qb4x-subham-mondals-projects-4d6994ac.vercel.app/",
       "https://book-list-qb4x.vercel.app" // replace
-=======
-      "https://book-list-qb4x-subham-mondals-projects-4d6994ac.vercel.app",
-      "https://book-list-qb4x.vercel.app"/ replace
->>>>>>> a4ab9c97dd04d962839d083b468b0872b0de9e22
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
