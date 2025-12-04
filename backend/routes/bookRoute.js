d45ce52021f998d9c41c7c9db0298b6cf5d4cb61
Mondal2002 <subhammondal735@gmail.com.com>
2025-12-04 17:24:11 +0530

    Updating Authentication process

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -314,7 +314,7 @@ router.post('/admin/login', async (req, res) => {
   }
 
 });
-router.get('/admin/allUsers',async(req,res)=>{
+router.get('/admin/allUsers',checkAdmin,async(req,res)=>{
   const alluser=await User.find();
   res.send(alluser);
 })
